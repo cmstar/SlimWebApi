31fb62080cfa97444bfe6f80df4825b123022641
cmstar <cosmo_sei@hotmail.com>
2014-07-11 10:59:05 +0800

    移除 ApiMethodParamInfoMap 中一处多余的判定。

--- src/cmstar.WebApi/ApiMethodParamInfoMap.cs
+++ src/cmstar.WebApi/ApiMethodParamInfoMap.cs
@@ -1,53 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Reflection;
 
 namespace cmstar.WebApi
 {
+    /// <summary>
+    /// 包含WebAPI方法的参数信息和有关操作。
+    /// </summary>
     public class ApiMethodParamInfoMap
     {
         private readonly Dictionary<string, ApiParamInfo> _paramTypeMap;
 
-        public ApiMethodParamInfoMap(MethodInfo methodInfo, IEqualityComparer<string> methodNameComparer = null)
+        /// <summary>
+        /// 初始化<see cref="ApiMethodParamInfoMap"/>的新实例。
+        /// </summary>
+        /// <param name="methodInfo">注册的方法。</param>
+        /// <param name="paramNameComparer">
+        /// 指定如何比较参数的名称。若设置为null，则使用<see cref="ApiEnvironment.DefaultMethodNameComparer"/>。
+        /// </param>
+        public ApiMethodParamInfoMap(MethodInfo methodInfo, IEqualityComparer<string> paramNameComparer = null)
         {
             ArgAssert.NotNull(methodInfo, "methodInfo");
             Method = methodInfo;
 
             var param = methodInfo.GetParameters();
-            var comparer = methodNameComparer ?? ApiEnvironment.DefaultMethodNameComparer;
+            var comparer = paramNameComparer ?? ApiEnvironment.DefaultMethodNameComparer;
             _paramTypeMap = new Dictionary<string, ApiParamInfo>(param.Length, comparer);
 
             for (var i = 0; i < param.Length; i++)
             {
                 var p = param[i];
                 var paramName = p.Name;
-                var paramType = p.ParameterType;
-
-                if (paramType.IsSubclassOf(typeof(IConvertible)))
-                {
-                    var msg = string.Format(
-                        "The parameter \"{0}\" (type {1}) of method {2} cannot be convert from the query string.",
-                        paramName, paramType, methodInfo.Name);
-                    throw new ArgumentException(msg, "methodInfo");
-                }
-
                 var paramInfo = new ApiParamInfo(i, p);
                 _paramTypeMap.Add(paramName, paramInfo);
             }
         }
 
+        /// <summary>
+        /// 获取当前实例所关联的<see cref="MethodInfo"/>。
+        /// </summary>
         public MethodInfo Method { get; private set; }
 
+        /// <summary>
+        /// 获取参数名称与<see cref="ApiParamInfo"/>的序列。
+        /// </summary>
         public IEnumerable<KeyValuePair<string, ApiParamInfo>> ParamInfos
         {
             get { return _paramTypeMap; }
         }
 
+        /// <summary>
+        /// 获取方法中参数的数量。
+        /// </summary>
         public int ParamCount
         {
             get { return _paramTypeMap.Count; }
         }
 
+        /// <summary>
+        /// 获取具有指定名称的<see cref="ApiParamInfo"/>。
+        /// </summary>
+        /// <param name="name">名称。</param>
+        /// <param name="paramInfo">
+        /// 输出参数。当方法返回<c>true</c>时为具有指定名称的<see cref="ApiParamInfo"/>的实例。
+        /// </param>
+        /// <returns><c>true</c>当指定的名称存在；否则为<c>false</c>。</returns>
         public bool TryGetParamInfo(string name, out ApiParamInfo paramInfo)
         {
             return _paramTypeMap.TryGetValue(name, out paramInfo);
