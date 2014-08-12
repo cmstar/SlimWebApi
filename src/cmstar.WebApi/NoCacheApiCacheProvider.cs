fc7cab02a1307ce6bff1a8ff60e23f8a85b44007
cmstar <cosmo_sei@hotmail.com>
2014-08-12 13:42:43 +0800

    增加一个不提供缓存实现的缓存提供器。

---
+++ src/cmstar.WebApi/NoCacheApiCacheProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cmstar.WebApi
+{
+    /// <summary>
+    /// 一个空的缓存提供器，实际上不提供任何缓存实现。
+    /// </summary>
+    public class NoCacheApiCacheProvider : IEnumerableApiCacheProvider
+    {
+        public object Get(string key)
+        {
+            return null;
+        }
+
+        public void Add(string key, object value, TimeSpan expiration)
+        {
+        }
+
+        public object Set(string key, object value, TimeSpan expiration)
+        {
+            return null;
+        }
+
+        public void Remove(string key)
+        {
+        }
+
+        public IEnumerable<KeyValuePair<string, object>> KeyValues(string prefix)
+        {
+            return new KeyValuePair<string, object>[0];
+        }
+
+        public void Clear(string prefix)
+        {
+        }
+    }
+}
