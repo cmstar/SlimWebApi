9b494eb040cc38dc13ac771265027b5120aae308
cmstar <cosmo_sei@hotmail.com>
2019-04-10 16:14:46 +0800

    将内部类 SlimApiHttpHandler.SlimApiReuqestState 提到外面以便后续扩展。

--- src/cmstar.WebApi/Slim/SlimApiHttpHandler.cs
+++ src/cmstar.WebApi/Slim/SlimApiHttpHandler.cs
@@ -538,33 +538,5 @@ namespace cmstar.WebApi.Slim
                 }
             }
         }
-
-        /// <summary>
-        /// 保存请求处理过程中的数据。
-        /// </summary>
-        protected class SlimApiRequestState
-        {
-            /// <summary>
-            /// 当前请求指定的方法入口的名称。
-            /// </summary>
-            public string MethodName;
-
-            /// <summary>
-            /// 当前请求指定的回调函数的名称。
-            /// 若指定了此名称，则请求是一个JSONP请求。
-            /// </summary>
-            public string CallbackName;
-
-            /// <summary>
-            /// 当前请求指定的请求参数的格式，如 json、get。
-            /// </summary>
-            public string RequestFormat;
-
-            /// <summary>
-            /// 返回结果是否使用类型为 text/plain 的 Content-Type 域。
-            /// 若为 false，则根据具体请求给定：JSONP 请求使用 text/javascript；JSON 格式则为 application/json。
-            /// </summary>
-            public bool UsePlainText;
-        }
     }
 }
---
+++ src/cmstar.WebApi/Slim/SlimApiRequestState.cs
@@ -0,0 +1,32 @@
+﻿namespace cmstar.WebApi.Slim
+{
+    /// <summary>
+    /// 记录<see cref="SlimApiHttpHandler"/>处理的一个请求中的状态。
+    /// </summary>
+    public class SlimApiRequestState
+    {
+        /// <summary>
+        /// 当前请求指定的方法入口的名称。
+        /// </summary>
+        public string MethodName;
+
+        /// <summary>
+        /// 当前请求指定的回调函数的名称。
+        /// 若指定了此名称，则请求是一个JSONP请求。
+        /// </summary>
+        public string CallbackName;
+
+        /// <summary>
+        /// 当前请求指定的请求参数的格式，如 json、get。
+        /// </summary>
+        public string RequestFormat;
+
+        /// <summary>
+        /// 返回结果是否使用类型为 text/plain 的 Content-Type 域。
+        /// 若为 false，则根据具体请求给定：JSONP 请求使用 text/javascript；JSON 格式则为 application/json。
+        /// </summary>
+        public bool UsePlainText;
+
+        public bool IsJsonpRequest => !string.IsNullOrEmpty(CallbackName);
+    }
+}
